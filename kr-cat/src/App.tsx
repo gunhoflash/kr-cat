05183dd2a41e03eecf82b99851fafaf6a2753ec3
dewy.i <dewy.i@kakaocorp.com>
2021-09-05 17:39:11 +0900

    Separate Timer

--- kr-cat/src/App.tsx
+++ kr-cat/src/App.tsx
@@ -1,22 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
-import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
-const birthday = dayjs('1996-10-24');
-const dayCounts = [
-  12500,
-  12345,
-  12000,
-  11500,
-  11111,
-  11000,
-  10500,
-  10001,
-  10000,
-  9999,
-  9876,
-  9500,
-];
+import Timer from './components/Timer';
 
 const baseUrl = "https://kr-carrot.herokuapp.com/api";
 const LOL_IMG_BASE = 'http://ddragon.leagueoflegends.com/cdn/11.15.1/img';
@@ -25,39 +9,12 @@ const xmlHttpRequest = new XMLHttpRequest();
 const getLOLImgPath = (v: string, subpath: string) => `${LOL_IMG_BASE}/${subpath}/${v}.png`;
 
 function App() {
-  const [now, setNow] = useState(0);
-
   const [apiKey, setApiKey] = useState('');
   const [LOLInfo, setLOLInfo] = useState<any>(null);
   const [histories, setHistories] = useState([]);
   const [isShowAPIInput, setIsShowAPIInput] = useState(false);
 
-  const requestAnimationFrameRef = useRef<number>();
-
-  const dayCount = dayCounts.reduce((prevDayCount, dayCount) => {
-    const anniversary = birthday.add(dayCount - 1, 'day');
-    const millisecond = anniversary.diff(now);
-    if (millisecond > 0) return dayCount;
-    if (millisecond > - 18 * 60 * 60 * 1000) return dayCount;
-    return prevDayCount;
-  }, dayCounts[0]);
-
-  const anniversary = birthday.add(dayCount - 1, 'day');
-  const millisecond = anniversary.diff(now);
-
-  const day    = millisecond > 0 ? Math.floor(millisecond / 1000 / 60 / 60 / 24) : 0;
-  const hour   = millisecond > 0 ? Math.floor(millisecond / 1000 / 60 / 60) % 24 : 0;
-  const minute = millisecond > 0 ? Math.floor(millisecond / 1000 / 60) % 60 : 0;
-  const second = millisecond > 0 ? Math.floor(millisecond / 1000) % 60 : 0;
-
-  function showRemainedDate() {
-    setNow(dayjs().valueOf());
-    requestAnimationFrameRef.current = requestAnimationFrame(showRemainedDate);
-  }
-
   useEffect(() => {
-    requestAnimationFrameRef.current = requestAnimationFrame(showRemainedDate);
-
     xmlHttpRequest.open("GET", baseUrl + "/summoner/아트런/3", true); // 하드코딩 ㅈㅅ
     xmlHttpRequest.send();
     xmlHttpRequest.onload = () => {
@@ -71,8 +28,6 @@ function App() {
         setIsShowAPIInput(true);
       }
     }
-
-    return () => cancelAnimationFrame(requestAnimationFrameRef.current as number);
   }, []);
 
   // api key 등록
@@ -85,25 +40,7 @@ function App() {
 
   return (
     <div>
-      <div>
-        <div className="subtitle">
-          {millisecond > 0
-            ? `${dayCount}까지`
-            : `${dayCount}: ${anniversary.format('YYYY-MM-DD')}`
-          }
-        </div>
-        <div>
-          {millisecond > 0 ? (
-            <>
-              {!!(day                            ) && <p className="title">{day}일</p>}
-              {!!(day || hour                    ) && <p className="title">{('0' + hour).slice(-2)}시간</p>}
-              {!!(day || hour || minute          ) && <p className="title">{('0' + minute).slice(-2)}분</p>}
-              {!!(day || hour || minute || second) && <p className="title">{('0' + second).slice(-2)}초</p>}
-              <p className="title">{('00' + millisecond).slice(-3)}</p>
-            </>
-          ) : 'Congratulations!'}
-        </div>
-      </div>
+      <Timer />
 
       {/* lol */}
       {LOLInfo && (
---
+++ kr-cat/src/components/Timer.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+
+const birthday = dayjs('1996-10-24');
+const dayCounts = [
+  12500,
+  12345,
+  12000,
+  11500,
+  11111,
+  11000,
+  10500,
+  10001,
+  10000,
+  9999,
+  9876,
+  9500,
+];
+
+const TitleText = ({text}: {text: string}) => useMemo(() => (
+  <p className="title">{text}</p>
+), [text]);
+
+const SubTitle = ({isTheDay, dayCount, anniversary}: {isTheDay: boolean; dayCount: number; anniversary: Dayjs}) => useMemo(() => (
+  <div className="subtitle">
+    {isTheDay
+      ? `${dayCount}: ${anniversary.format('YYYY-MM-DD')}`
+      : `${dayCount}까지`}
+  </div>
+), [isTheDay, dayCount]);
+
+const Timer = () => {
+  const [now, setNow] = useState(0);
+
+  const requestAnimationFrameRef = useRef<number>();
+
+  const dayCount = dayCounts.reduce((prevDayCount, dayCount) => {
+    const anniversary = birthday.add(dayCount - 1, 'day');
+    const millisecond = anniversary.diff(now);
+    if (millisecond > 0) return dayCount;
+    if (millisecond > - 18 * 60 * 60 * 1000) return dayCount;
+    return prevDayCount;
+  }, dayCounts[0]);
+
+  const anniversary = birthday.add(dayCount - 1, 'day');
+  const millisecond = anniversary.diff(now);
+  const isTheDay = millisecond <= 0;
+
+  const day    = isTheDay ? 0 : Math.floor(millisecond / 1000 / 60 / 60 / 24);
+  const hour   = isTheDay ? 0 : Math.floor(millisecond / 1000 / 60 / 60) % 24;
+  const minute = isTheDay ? 0 : Math.floor(millisecond / 1000 / 60) % 60;
+  const second = isTheDay ? 0 : Math.floor(millisecond / 1000) % 60;
+
+  function showRemainedDate() {
+    setNow(dayjs().valueOf());
+    requestAnimationFrameRef.current = requestAnimationFrame(showRemainedDate);
+  }
+
+  useEffect(() => {
+    requestAnimationFrameRef.current = requestAnimationFrame(showRemainedDate);
+    return () => cancelAnimationFrame(requestAnimationFrameRef.current as number);
+  }, []);
+
+  return (
+    <>
+      <SubTitle isTheDay={isTheDay} dayCount={dayCount} anniversary={anniversary} />
+      <div>
+        {isTheDay ? 'Congratulations!' : (
+          <>
+            {!!(day                            ) && <TitleText text={`${day}일`} />}
+            {!!(day || hour                    ) && <TitleText text={`${('0' + hour).slice(-2)}시간`} />}
+            {!!(day || hour || minute          ) && <TitleText text={`${('0' + minute).slice(-2)}분`} />}
+            {!!(day || hour || minute || second) && <TitleText text={`${('0' + second).slice(-2)}초`} />}
+            <TitleText text={('00' + millisecond).slice(-3)} />
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Timer;
