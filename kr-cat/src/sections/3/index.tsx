6ce6576f93b4032e6b96fe2655af8bfd5e5dc39a
dewy.i <dewy.i@kakaocorp.com>
2022-01-01 01:26:21 +0900

    Handle error

--- kr-cat/src/sections/3/index.tsx
+++ kr-cat/src/sections/3/index.tsx
@@ -7,12 +7,16 @@ function Section() {
   const [winStatus, setWinStatus] = useState<string>('(불러오는 중)');
 
   const requestWinStatus = async () => {
-    const response = await fetch(
-      `${URL_BASE}/user/data`,
-      {method: 'GET'}
-    );
-    const data = (await response.text()) as string;
-    setWinStatus(data);
+    try {
+      const response = await fetch(
+        `${URL_BASE}/user/data`,
+        {method: 'GET'}
+      );
+      const data = (await response.text()) as string;
+      setWinStatus(data);
+    } catch (e) {
+      setWinStatus('-');
+    }
   };
 
   useEffect(() => {
